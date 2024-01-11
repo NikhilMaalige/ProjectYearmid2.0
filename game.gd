b731454031fa9aded2df92b5145929e2a9d68cd4
lieman27 <lieman27@bergen.org>
2024-01-10 21:19:30 -0500

    name

--- game.gd
+++ game.gd
@@ -11,10 +11,5 @@ func _process(delta):
 	pass
 
 
-
 func _on_quit_pressed():
 	get_tree().quit() # Replace with function body.
-
-
-func _on_play_pressed():
-	get_tree().change_scene_to_file("res://game.tscn") # Replace with function body.
