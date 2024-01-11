e9a967bf7931dc82164e3e4df4b2179d04d72431
lieman27 <lieman27@bergen.org>
2024-01-11 12:23:22 -0500

    fix game window thing i forgot what its called but its fixed now but we need to fix the gravity thing

--- main.gd
+++ main.gd
@@ -16,5 +16,7 @@ func _on_quit_pressed():
 	get_tree().quit() # Replace with function body.
 
 
+
+
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes:game.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/game.tscn") # Replace with function body.
