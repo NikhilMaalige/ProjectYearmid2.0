b282baefb6d79d8afe197ecffc75d43b14a2ef83
lopsided6100 <jujuzhu2009@gmail.com>
2024-01-25 15:17:09 -0500

    settings and back
    my first ever commit!

--- main.gd
+++ main.gd
@@ -20,3 +20,7 @@ func _on_quit_pressed():
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn") # Replace with function body.
+
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://scenes/settings.tscn") # Replace with function body.
--- scenes/settings.gd
+++ scenes/settings.gd
@@ -10,4 +10,5 @@ func _ready():
 func _process(delta):
 	pass
 
-
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn") # Replace with function body.
