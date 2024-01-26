2a637a607f58022365786ddaacf2803ab0bb4e9f
lieman27 <lieman27@bergen.org>
2024-01-26 11:22:55 -0500

    fixed enemy being god but theres an animation issue
    at least it works and isnt a god enemy

--- scenes/enermy_1.gd
+++ scenes/enermy_1.gd
@@ -5,23 +5,26 @@ var player_chase = false
 var player = null
 @onready var animations_enemy = $Enermy1Animation
 
+
 func _physics_process(delta):
+		
 	if player_chase:
-		position += (player.position - position)/speed2
-	
-		var moveDirection2 = Input.get_vector("ui_left" , "ui_right", "ui_up" , "ui_down")
-		velocity = moveDirection2*speed2
+		position += (player.position - position)/speed2		
 		
-		
-		if velocity.length() != 0:
-			var direction = "down"
-			if velocity.x <0: direction = "left"
-			elif velocity.x > 0: direction = "right"
-			elif velocity.y < 0: direction = "up"
+		if (player.position - position).x != 0:
+			var direction = " "
+			if (player.position - position).x <0: direction = "left"
+			elif (player.position - position).x > 0: direction = "right"
+			animations_enemy.play("enemy1_walk_" + direction)
+		elif (player.position - position).y != 0:
+			var direction = " "
+			if (player.position.y - position.y).y < 0: direction = "up"
+			if (player.position.y - position.y).y > 0: direction = "down"
 			animations_enemy.play("enemy1_walk_" + direction)
 		else:
 			animations_enemy.stop()
-
+	move_and_slide()
+	
 func _on_detection_area_body_entered(body):
 	player = body
 	player_chase = true
