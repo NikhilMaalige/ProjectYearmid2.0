68840a3a1a89f7ae834ab26d537184124714041a
lopsided6100 <jujuzhu2009@gmail.com>
2024-01-30 16:00:37 -0500

    juuuuust one more
    figured id get tired of the audio so i need a way to make it STOPPPPP

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var volume: float = 1.0;
+var testingbool: bool = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,11 +12,17 @@ func _process(delta):
 	pass
 
 func _on_back_pressed():
+	$audio_tester.stop();
 	get_tree().change_scene_to_file("res://scenes/main.tscn") # Replace with function body.
 
 func _on_test_pressed():
-	$audio_tester.play()  # Replace with function body.
-
+	if testingbool:
+		$audio_tester.stop();
+		testingbool = false;
+	else:
+		$audio_tester.play();
+		testingbool = true;
+	# Replace with function body.
 
 func _on_volume_slider_value_changed(value):
 	pass # Replace with function body.
