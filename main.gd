b3468c80476f320c03c368439d78787023da9126
lopsided6100 <jujuzhu2009@gmail.com>
2024-02-01 15:22:51 -0500

    totally not boosting my commits
    idk why but it says ive afected "openbookgodot2024.png.import" but idk what that is

--- main.gd
+++ main.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$music.play(); # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,8 +16,10 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 func _on_settings_pressed():
+	$music.play();
 	get_tree().change_scene_to_file("res://scenes/settings.tscn") 
 
 
 func _on_play_pressed():
+	$music.play();
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
