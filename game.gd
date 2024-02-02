7137fc27f8108ab25a721a4ebcd818a3447df79a
lieman27 <lieman27@bergen.org>
2024-02-02 08:47:47 -0500

    hopefully its not gonna collide but i added a book thingy

--- game.gd
+++ game.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 
+@onready var popup = $Window
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$"overworld-day-player".play();
+	popup.hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +18,13 @@ func _process(delta):
 func _on_return_menu_pressed():
 	$"overworld-day-player".stop();
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	
+	
 
 
-func _on_node_2d_pressed():
-	pass # Replace with function body.
+func _on_window_close_requested():
+	popup.hide()
+
+
+func _on_purplebook_pressed():
+	popup.show()
