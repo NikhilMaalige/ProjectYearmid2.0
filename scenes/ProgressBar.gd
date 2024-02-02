f231f04b32a4f5203e239c7771d0d9ed973982ba
lieman27 <lieman27@bergen.org>
2024-02-01 22:07:01 -0500

    so i made the menu button thing, and also the hurt blink thing and knockback which fixes some bugs that wee had and now its a lot easier for us to do this because some of the code doesnt have to get fixed anymore so we are 1 step closer to having a working midyear [project game thigny

--- scenes/ProgressBar.gd
+++ scenes/ProgressBar.gd
@@ -3,6 +3,8 @@ extends ProgressBar
 @export var player: Player
 
 func _ready():
+	#player.healthChanged.connect(update)
+	#update()
 	pass
 
 func update(): 
--- scenes/player.gd
+++ scenes/player.gd
@@ -2,19 +2,29 @@ extends CharacterBody2D
 
 class_name Player 
 
-signal healthChanged
+#signal healthChanged
+
 
 @export var speed: int = 100
 @onready var animations = $PlayerAnimation
 @onready var effects = $Effects
+@onready var hurtTimer = $hurtTimer
 
 @export var maxHealth = 30
 @onready var currentHealth: int = maxHealth
+
+@export var knockbackPower: int = 500
+
+var isHurt: bool = false
+var enemyCollisions = []
 
 var health = 100
 
 var lastAnimDirection = "down"
 var isAttacking: bool = false
+
+func _ready():
+	effects.play("RESET")
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left" , "ui_right", "ui_up" , "ui_down")
@@ -47,9 +57,34 @@ func _physics_process(delta):
 	handleInput()
 	move_and_slide()
 	updateAnimation()
+	if !isHurt: 
+		for enemyArea in enemyCollisions:
+			hurtByEnemy(enemyArea)
 
-
+func hurtByEnemy(area):
+	currentHealth -= 3
+	if currentHealth <= 0:
+		get_tree().change_scene_to_file("res://scenes/loss.tscn")
+		
+	isHurt = true
+	#healthChanged.emit()	
+	
+	knockback(area.get_parent().velocity)
+	effects.play("hurtBlink")
+	hurtTimer.start()
+	await hurtTimer.timeout
+	effects.play("RESET")
+	isHurt = false
+	print_debug(currentHealth)
+		
 func _on_hurt_box_area_entered(area):
-	if area.name == "HitBox": 
-		currentHealth -= 3
-		print_debug(currentHealth) # Replace with function body.
+	if area.name == "HitBox":
+		enemyCollisions.append(area)
+		
+func knockback(enemyVelocity: Vector2):
+	var knockbackDirection = (enemyVelocity-velocity).normalized() * knockbackPower 
+	velocity = knockbackDirection
+	move_and_slide()
+
+func _on_hurt_box_area_exited(area):
+	enemyCollisions.erase(area)
